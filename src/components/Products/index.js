7f0d74063b745abd7d3c32727e471675b881c2ef
sainithin <nithinreddy0507@gmail.com>
2024-08-01 13:48:31 +0000

    first commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,24 @@
+// Write your JS code here
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import Header from '../Header'
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+export default Products
